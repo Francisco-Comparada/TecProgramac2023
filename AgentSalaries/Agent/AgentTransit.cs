8a8dcc67598b8a80fcc6b4685a0f281d9282a10a
francisco comparada <francisco.comparada@gmail.com>
2023-10-05 21:07:19 -0300

    Armado de estructura del programa, clases flata implemenentar metodos

---
+++ AgentSalaries/Agent/AgentTransit.cs
@@ -0,0 +1,20 @@
+﻿using AgentSalaries.Administrative;
+
+namespace AgentSalaries.Agent;
+
+public class AgentTransit : Agent
+{
+    public List<TrafficFines> finesIssued { get; set; }
+
+    public AgentTransit(int registrationNumber, string name, string position, DateTime dateOfAdmission,
+        BaseSalary baseSalary, List<TrafficFines> finesIssued) : base(registrationNumber,
+        name, position, dateOfAdmission, baseSalary)
+    {
+        this.finesIssued = finesIssued;
+    }
+
+    public override double CalculateSalary()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AgentSalaries/Agent/TrafficFines.cs
@@ -0,0 +1,15 @@
+﻿namespace AgentSalaries.Agent;
+
+public class TrafficFines
+{
+    public double fineAmount { get; set; }
+    public string infractionType { get; set; }
+    public AgentTransit agent { get; set; }
+
+    public TrafficFines(double fineAmount, string infractionType, AgentTransit agent)
+    {
+        this.fineAmount = fineAmount;
+        this.infractionType = infractionType;
+        this.agent = agent;
+    }
+}
