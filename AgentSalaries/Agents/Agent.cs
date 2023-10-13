3d7f53e9c4bcf94de98d81ee52d6c0396cec8aa9
Mati Brizuela <mbrizuela96@gmail.com>
2023-10-13 18:48:18 -0300

    Modifique los atributos y metodos de las clases

---
+++ AgentSalaries/Agents/Agent.cs
@@ -0,0 +1,36 @@
+﻿namespace AgentSalaries.Agents
+{
+    // Clase padre para que representa de forma general a los agentes 
+    public abstract class Agent
+    {
+        // Atributos
+        protected int registrationNumber;
+        protected string name;
+        protected string position;
+        protected DateTime dateOfAdmission;
+
+
+
+        /* Por mas que la clase sea abstracta, defino el constructor para inicializar
+         * el objeto todo en una linea y no tener que hacerlo con un set. */
+
+        public Agent(int registrationNumber, string name, string position, DateTime dateOfAdmission
+           )
+        {
+            this.registrationNumber = registrationNumber;
+            this.name = name;
+            this.position = position;
+            this.dateOfAdmission = dateOfAdmission;
+
+        }
+
+        // Propiedades
+        public int RegistrationNumber { get => registrationNumber; set => registrationNumber = value; }
+        public string Name { get => name; set => name = value; }
+        public string Position { get => position; set => position = value; }
+        public DateTime DateOfAdmission { get => dateOfAdmission; set => dateOfAdmission = value; }
+
+        //Polimorfismo para que sus clases hijas tengan el mismo nombre del metodo pero distintos codigos 
+        public abstract double CalculateSalary();
+    }
+}
